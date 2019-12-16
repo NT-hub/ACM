ba1a8ccaa58fb702d61c068a6657971dd3abe6ad
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-17 07:31:09 +1030

    fix this error-->.vs/ACM/v16/Server/sqlite3/db.lock and this is a solution:https://stackoverflow.com/questions/47460039/vs-2017-git-local-commit-db-lock-error-on-every-commit/47472811

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        // add private access modifier to make sure no code
+        // can access to it.
+        private string _lastName;
+
+        // C# Property(full property syntax)
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        // when there is no need to add logics into the getter and setter,
+        // this a good wat to initials it.(auto  implemented property syntax)
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+
+        // in this case we want to adding the ID but never setting it.
+        // but only this class can set it.
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+
+        }
+    }
+}
