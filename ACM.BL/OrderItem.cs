b9186748e1660a597c4613937cd8af2ecc711a46
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-17 13:33:26 +1030

    Added Order class

--- ACM.BL/OrderItem.cs
+++ ACM.BL/OrderItem.cs
@@ -8,5 +8,50 @@ namespace ACM.BL
 {
     class OrderItem
     {
+        public OrderItem()
+        {
+                
+        }
+        public OrderItem(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public int OrderId { get; private set; }
+
+        // DateTimeOffset is good to set different time zones.
+        public DateTimeOffset? OrderDate { get; set; }
+        public string Address { get; set; }
+        public bool Validate()
+        {
+            var isValid = true;
+            // assumption is both of them are required and need to be validated both.
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        /// <summary>
+        /// Retrieve one order.
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public Order Retrieve(int orderId)
+        {
+            //code that retrieves the defined order
+            return new Order();
+        }
+
+        /// <summary>
+        /// Retrieve all orders.
+        /// </summary>
+        /// <returns></returns>
+        public List<Order> Retrieve()
+        {
+            //code that retrieve all of the orders
+            return new List<Order>();
+        }
     }
 }
