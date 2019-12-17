befe5896ee8a732138de6a5b0bf9ab021591ca4d
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-17 14:02:56 +1030

    added OrderItem class

--- ACM.BL/OrderItem.cs
+++ ACM.BL/OrderItem.cs
@@ -12,46 +12,38 @@ namespace ACM.BL
         {
                 
         }
-        public OrderItem(int orderId)
+        public OrderItem(int orderItemId)
         {
-            OrderId = orderId;
+            OrderItemId = orderItemId;
         }
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quality { get; set; }
 
-        public int OrderId { get; private set; }
-
-        // DateTimeOffset is good to set different time zones.
-        public DateTimeOffset? OrderDate { get; set; }
-        public string Address { get; set; }
-        public bool Validate()
-        {
-            var isValid = true;
-            // assumption is both of them are required and need to be validated both.
-            if (OrderDate == null) isValid = false;
-            return isValid;
-        }
         public bool Save()
         {
             return true;
         }
         /// <summary>
-        /// Retrieve one order.
+        /// Retrieve one order item.
         /// </summary>
-        /// <param name="orderId"></param>
+        /// <param name="orderItemId"></param>
         /// <returns></returns>
-        public Order Retrieve(int orderId)
+        public OrderItem Retrieve(int orderItemId)
         {
-            //code that retrieves the defined order
-            return new Order();
+            //code that retrieves the defined order.
+            return new OrderItem();
         }
 
         /// <summary>
-        /// Retrieve all orders.
+        /// Retrieve all order items.
         /// </summary>
         /// <returns></returns>
-        public List<Order> Retrieve()
+        public List<OrderItem> Retrieve()
         {
-            //code that retrieve all of the orders
-            return new List<Order>();
+            //code that retrieve all of the order items.
+            return new List<OrderItem>();
         }
     }
 }
