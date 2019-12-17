d8bbbb65d4f9193f91f1f57391893ef2159e1018
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-17 13:19:24 +1030

    added Product class

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Product
+    {
+
+        public Product()
+        {
+                
+        }
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal? CurrentPrice { get; set; }
+        public bool Validate()
+        {
+            var isValid = true;
+            // assumption is both of them are required and need to be validated both.
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+            return isValid;
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        /// <summary>
+        /// Retrieve one product.
+        /// </summary>
+        /// <param name="productid"></param>
+        /// <returns></returns>
+        public Product Retrieve(int productId)
+        {
+            //code that retrieves the defined product
+            return new Product();
+        }
+
+        /// <summary>
+        /// Retrieve all products.
+        /// </summary>
+        /// <returns></returns>
+        public List<Product> Retrieve()
+        {
+            //code that retrieve all of the products
+            return new List<Product>();
+        }
+    }
+}
