c560bfdfec9abf1cd4483229e72fbe1b1c92c6b8
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-18 11:31:26 +1030

    Implementing a composition relationship between Order-->Customer/Order-->Address/Order-->OrderItem

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -8,20 +8,25 @@ namespace ACM.BL
 {
    public class Order
     {
-        public Order()
+        public Order(): this(0)
         {
                 
         }
         public Order(int orderId)
         {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
+        public int CustomerId { get; set; }
+        public int ShippingAddressId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
         public int OrderId { get; private set; }
 
         // DateTimeOffset is good to set different time zones.
         public DateTimeOffset? OrderDate { get; set; }
         public string Address { get; set; }
+
         public bool Validate()
         {
             var isValid = true;
